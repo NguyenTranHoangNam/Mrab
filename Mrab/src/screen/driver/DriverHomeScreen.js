f6aae6cae28aa82126768062de8ea28c571ed57a
haolk <haolk@vng.com.vn>
2019-05-22 21:59:08 +0700

    export color const

--- Mrab/src/screen/driver/DriverHomeScreen.js
+++ Mrab/src/screen/driver/DriverHomeScreen.js
@@ -1,10 +1,14 @@
 import React, { Component } from 'react';
 import { View } from 'react-native';
+import { BACKGROUND_COLOR } from '../../utils';
+
 export default class DriverHomeScreen extends Component {
     state = {  }
     render() {
         return (
-            <View style={{flex:1,backgroundColor:"blue"}}></View>
+            <View style={{flex:1,backgroundColor: BACKGROUND_COLOR}}>
+
+            </View>
         );
     }
 }
